f7afa80d5efa004940099f5d542caf2e12356f85
sebastian <seba@dealingapp.com>
2020-11-08 10:09:41 -0300

    Test

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
 docker-compose -f home/ec2-user/iic2173-proyecto-semestral-grupo-14/production.yml up -d
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,2 @@
-#!/bin/bash
 docker-compose -f home/ec2-user/production.yml down
 docker stop $(docker ps -a -q)
