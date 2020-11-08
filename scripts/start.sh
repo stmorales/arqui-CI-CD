4f435d9d19967fadc9e25e0021503998900376bd
sebastian <seba@dealingapp.com>
2020-11-08 20:57:36 -0300

    Change path

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 # sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 # sudo chmod +x /opt/bin/docker-compose
 
+cd ..
 cd home/ec2-user/iic2173-proyecto-semestral-grupo-14
 ls
 docker-compose -f production.yml up -d
