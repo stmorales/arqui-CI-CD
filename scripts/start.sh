51de66c57cbdec25e57e74f7971fea62fe8f136c
sebastian <seba@dealingapp.com>
2020-11-08 20:37:15 -0300

    Change path

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,31 +1,12 @@
 # sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
 # sudo chmod +x /opt/bin/docker-compose
-echo 1
-ls
+
 cd ..
-echo 2
-ls
 cd ..
-echo 3
-ls
 cd ..
-echo 4
-ls
 cd ..
-echo 5
-ls
 cd ..
-echo 6
-ls
 cd ..
-echo 7
 ls
-cd ..
-echo 8
-ls
-cd ..
-echo 9
-ls
-cd ..
 
 docker-compose -f iic2173-proyecto-semestral-grupo-14/production.yml up -d
