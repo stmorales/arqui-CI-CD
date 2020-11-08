e9cae0309e7b56e29b1203a9d495d794d63c7326
sebastian <seba@dealingapp.com>
2020-11-08 18:24:19 -0300

    Test

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.0/docker-compose-`uname -s`-`uname -m` | sudo tee /usr/local/bin/docker-compose > /dev/null
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /opt/bin/docker-compose
 docker-compose -f home/ec2-user/iic2173-proyecto-semestral-grupo-14/production.yml up -d
