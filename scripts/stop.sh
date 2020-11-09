c9ee11a7560bf5335b284ca433f4c00069fa66a9
sebastian <seba@dealingapp.com>
2020-11-08 21:59:18 -0300

    SOme changes

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,2 +1,5 @@
-docker-compose -f home/ec2-user/iic2173-proyecto-semestral-grupo-14/production.yml down
-# docker stop $(docker ps -a -q)
+cd ..
+cd ..
+cd /home/ec2-user/iic2173-proyecto-semestral-grupo-14
+docker-compose -f production.yml down
+docker stop $(docker ps -a -q)
